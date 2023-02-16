91bfa42e4a9185d3a33cc0c62febd5e8935cc8e7
Joan Ferrel <ferrel007_2000@hotmail.com>
2023-02-15 23:55:56 -0500

    Primera carga

---
+++ 01-IntroToCS/demo-clase/demo.js
@@ -0,0 +1,25 @@
+var a = 10;
+var b = 15;
+
+var suma = 10 + 15;
+
+// * BINARIO A DECIMAL
+// 1 0 1 1
+
+// 1 * 2 ** 0 = 1
+// 1 * 2 ** 1 = 2
+// 0 * 2 ** 2 = 0
+// 1 * 2 ** 3 = 8
+// ----------------
+//              11
+
+// * DECIMAL A BINARIO
+// 11
+
+// 11 / 2 = 5 => R  1
+// 5  / 2 = 2 => R  1
+// 2  / 2 = 1 => R  0
+// 1  / 2 = 0 => R  1
+
+// ! CTRL + K + C = COMENTAR
+// ! CTRL + K + U = DESCOMENTAR
